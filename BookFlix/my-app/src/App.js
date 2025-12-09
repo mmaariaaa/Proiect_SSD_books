f1dff3dc2f9cbb999abb238a606591e38c6359d9
mmaariaaa <miruna.oancea@gmail.com>
2025-12-09 21:19:00 +0200

    Add navigation links to App component

--- BookFlix/my-app/src/App.js
+++ BookFlix/my-app/src/App.js
@@ -1,19 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchPage from "./SearchPage";
 import ItemDetails from "./ItemDetails";
-import AddItem from "./AddItem"; // your AddItem.js
-import PersonalList from "./PersonalList"; // optional
+import AddItem from "./AddItem";
+import PersonalList from "./PersonalList";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/item/:id" element={<ItemDetails />} />
-        <Route path="/add" element={<AddItem />} />
-        <Route path="/my-list" element={<PersonalList />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <nav style={{ padding: "10px", marginBottom: "20px", background: "#eee" }}>
+          <Link to="/" style={{ marginRight: "15px" }}>Search</Link>
+          <Link to="/add" style={{ marginRight: "15px" }}>add Item</Link>
+          <Link to="/my-list">My List</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+
+          <Route path="/item/:id" element={<ItemDetails />} />
+
+          <Route path="/add" element={<AddItem />} />
+          <Route path="/my-list" element={<PersonalList />} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
