6a43689bf53344cb33350157b764e212061c66ad
mmaariaaa <miruna.oancea@gmail.com>
2026-01-11 17:44:26 +0200

    Update App.js

--- BookFlix/my-app/src/App.js
+++ BookFlix/my-app/src/App.js
@@ -5,27 +5,31 @@ import AddItem from "./AddItem";
 import PersonalList from "./PersonalList";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <header style={{ padding: "1rem", backgroundColor: "#f5f5f5" }}>
-        <nav>
-          <Link to="/" style={{ marginRight: "1rem" }}>Search</Link>
-          <Link to="/add" style={{ marginRight: "1rem" }}>Add Item</Link>
-          <Link to="/my-list">My List</Link>
-        </nav>
-      </header>
+    return (
+        <BrowserRouter>
+            <header style={{ padding: "1rem", backgroundColor: "#f5f5f5" }}>
+                <nav>
+                    <Link to="/" style={{ marginRight: "1rem" }}>
+                        Search
+                    </Link>
+                    <Link to="/add" style={{ marginRight: "1rem" }}>
+                        Add Item
+                    </Link>
+                    <Link to="/my-list">My List</Link>
+                </nav>
+            </header>
 
-      <main style={{ padding: "1rem" }}>
-        <Routes>
-          <Route path="/" element={<SearchPage />} />
-          <Route path="/item/:id" element={<ItemDetails />} />
-          <Route path="/add" element={<AddItem />} />
-          <Route path="/my-list" element={<PersonalList />} />
-          <Route path="*" element={<h2>404: Page Not Found</h2>} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+            <main style={{ padding: "1rem" }}>
+                <Routes>
+                    <Route path="/" element={<SearchPage />} />
+                    <Route path="/item/:id" element={<ItemDetails />} />
+                    <Route path="/add" element={<AddItem />} />
+                    <Route path="/my-list" element={<PersonalList />} />
+                    <Route path="*" element={<h2>404: Page Not Found</h2>} />
+                </Routes>
+            </main>
+        </BrowserRouter>
+    );
 }
 
 export default App;
